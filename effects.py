a7d59e8b87c8444873f38c4350ac2ea5fbc18214
SamuelSoNChino <samko.filip2@gmail.com>
2025-01-30 14:57:48 +0100

    Added ImageEffect

--- effects.py
+++ effects.py
@@ -5,6 +5,7 @@ import numpy as np
 
 
 Color = tuple[int, int, int]
+Position = tuple[float, float, float, float]
 
 
 class Efect():
@@ -61,4 +62,40 @@ class ShakyCamEffect(Efect):
             frame = np.roll(frame, shift_x, axis=0)
             frame = np.roll(frame, shift_y, axis=1)
             frame = cv.resize(frame, (frame.shape[1], frame.shape[0]))
+        return frame
+
+
+class ImageEffect(Efect):
+    def __init__(self, start: float, end: float,
+                 image_path: str, position: Position):
+        self.start = start
+        self.end = end
+        self.image_path = image_path
+        self.position = position  # (x_min, y_min, x_max, y_max)
+        self.image = cv.imread(image_path, cv.IMREAD_UNCHANGED)
+        if self.image is None:
+            print(f'ERROR: Image at: {image_path} not found.')
+
+    def apply(self, frame: MatLike, current_second: float) -> MatLike:
+        if self.start <= current_second < self.end:
+            if self.image is None:
+                return frame
+
+            frame_height, frame_width = frame.shape[:2]
+            x_min = round(self.position[0] * frame_width)
+            y_min = round(self.position[1] * frame_height)
+            x_max = round(self.position[2] * frame_width)
+            y_max = round(self.position[3] * frame_height)
+
+            target_width = x_max - x_min
+            target_height = y_max - y_min
+            resized_image = cv.resize(self.image,
+                                      (target_width, target_height))
+
+            if resized_image.shape[2] == 4:  # Check for alpha channel
+                mask = resized_image[:, :, 3] > 0
+                frame[y_min:y_max, x_min:x_max][mask] = \
+                    resized_image[:, :, :3][mask]
+            else:
+                frame[y_min:y_max, x_min:x_max] = resized_image
         return frame
